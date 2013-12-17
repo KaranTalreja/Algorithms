cf3bbf2e1bb5019e8a1a8ae7d69b4b3c3020b1ac
KaranTalreja <karantalreja91@gmail.com>
2013-12-18 00:36:31 +0530

    Code clean up

--- src/Clustering.cpp
+++ src/Clustering.cpp
@@ -47,7 +47,6 @@ int main() {
 		(*Graph)[j] = tempNode;
 		hash.insert(tr1::unordered_multimap<int,node*>::value_type((*Graph)[j]->Data,(*Graph)[j]));
 	}
-	//cout<< computeHamming((*Graph)[0]->Data,(*Graph)[1]->Data,noOfBits)<<endl;
 
 	UnionFind UF(Graph);
 	int retVal =0;
@@ -124,50 +123,7 @@ int main() {
 			}
 		}
 	}
-//	cout<<kk<<endl;
-	//cout<<noOfClusters<<endl;
 	cout<<"No of leaders " << UF.decompile() <<endl;
-//	tr1::unordered_set<int>::iterator itr = UF.hash2->begin();
-//	tr1::unordered_set<int>::iterator previtr = UF.hash2->begin();
-//	tr1::unordered_set<int>::iterator itr2 = UF.hash2->begin();
-//	for(;itr!=UF.hash2->end();itr++)
-//	{
-//		previtr = itr;
-//		itr++;
-//		for(itr2=itr;itr2!=UF.hash2->end();itr2++)
-//		{
-//			int first,tempFirst;
-//			itr = previtr;
-//			first = (*Graph)[(*itr)]->Data;
-//			tempFirst = (*Graph)[(*itr2)]->Data;
-//			int t = computeHamming(first,tempFirst,noOfBits);
-//		//	if(t != 1 && t != 2 )
-//				cout<<"Hamming "<<t<<" for "<<first<<" "<<tempFirst<<endl;
-//		}
-//
-//	}
-
-//	for(int i=0;i<noOfNodes;i++)
-//	{
-//		for(int j=i+1;j<noOfNodes;j++)
-//		{
-//			k++;
-//			first = (*Graph)[i]->Data;
-//			second =(*Graph)[j]->Data;
-//			int xOr = (first^second);
-//			if(computeHamming(first,second,noOfBits) <= 2)
-//			{
-//				int Firstleader = UF.Find((*Graph)[i+1]->Id);
-//				int Secondleader =UF.Find((*Graph)[j+1]->Id);
-//				if(Firstleader != Secondleader)
-//				{
-//					UF.Union(Firstleader+1,Secondleader+1);
-//					noOfClusters++;
-//				}
-//			}
-//		}
-//	}
-//	cout<<noOfClusters<<" "<<k<<endl;
 	for(int i = 0;i<noOfNodes ;i++)
 		delete ((*Graph)[i]);
 	return 0;
@@ -204,58 +160,3 @@ int main() {
 //	}
 //		}
 //	}
-//for(int i = 0; i<noOfEdges;i++)
-//{
-//	yylex();
-//	tempNodeStartVal = atoi(yytext);
-//	yylex();
-//	tempNodeEndVal = atoi(yytext);
-//	yylex();
-//	tempWeight = atoi(yytext);
-//	tempEdge = new edge();
-//	tempNodeStart = (NULL == (*Graph)[tempNodeStartVal-1]) ? new node(tempNodeStartVal) : (*Graph)[tempNodeStartVal-1];
-//	tempNodeEnd = (NULL == (*Graph)[tempNodeEndVal-1]) ? new node(tempNodeEndVal) : (*Graph)[tempNodeEndVal-1];
-//	tempEdge->first = tempNodeStart;
-//	tempEdge->second = tempNodeEnd;
-//	tempEdge->weight = tempWeight;
-//	(*Graph)[tempNodeStartVal-1] = ((*Graph)[tempNodeStartVal-1] == NULL ) ? tempNodeStart :(*Graph)[tempNodeStartVal-1];
-//	(*Graph)[tempNodeEndVal-1] = ((*Graph)[tempNodeEndVal-1] == NULL ) ? tempNodeEnd :(*Graph)[tempNodeEndVal-1];
-//	heap.insert(tempEdge);
-//}
-//
-//int noOfClusters = noOfNodes;
-//int requiredClusters = 4;
-//UnionFind UF(Graph);
-//while(noOfClusters != requiredClusters)
-//{
-//	tempEdge = heap.extractMin();
-//	tempNodeStartVal = tempEdge->first->Id;
-//	tempNodeEndVal = tempEdge->second->Id;
-//	int Firstleader = UF.Find(tempNodeStartVal);
-//	int Secondleader =UF.Find(tempNodeEndVal);
-//	if(Firstleader != Secondleader)
-//	{
-//		UF.Union(Firstleader,Secondleader);
-//		noOfClusters --;
-//	}
-//	delete tempEdge;
-//}
-//int maxSpacing;
-//while(1)
-//{
-//	tempEdge=heap.extractMin();
-//	tempNodeStartVal = tempEdge->first->Id;
-//	tempNodeEndVal = tempEdge->second->Id;
-//	if(UF.Find(tempNodeStartVal) != UF.Find(tempNodeEndVal))
-//	{
-//		maxSpacing = tempEdge->weight;
-//		delete tempEdge;
-//		break;
-//	}
-//	delete tempEdge;
-//}
-//cout<<maxSpacing<<endl;
-//for(int i = 0;i<noOfNodes ;i++)
-//	delete ((*Graph)[i]);
-//heap.freeHeap();
-//return 0;
